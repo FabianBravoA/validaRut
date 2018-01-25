6a6f8f047cd5c0f0447d1b907cf870da1db0254d
Fabián Alexis Bravo Abarca <fabian.bravo.abarca@gmail.com>
2018-01-25 11:52:46 -0300

    Test de DOM

--- src/rutdom.js
+++ src/rutdom.js
@@ -4,12 +4,28 @@ const RUT = require('./rut');
 function validarInput(idInput) {
     let theInput = $(`#${idInput}`);
     theInput.change(() => {
-        console.log(validar(theInput.val));
+        if (!validar(theInput.val)) {
+            putErrorImg(theInput);
+        } else {
+            deleteErrorImg();
+        }
     });
 }
 
 function validar(inputString) {
     return RUT.validaRut(inputString);
+}
+
+function putErrorImg(theInput) {
+    if (!$("#errorDoge")) {
+        theInput.parent.append("<img id='errorDoge' src=" + __dirname + "../assets/img/error.jpg" + "></img>")
+    }
+}
+
+function deleteErrorImg() {
+    if ($("#errorDoge")) {
+        $("#errorDoge").remove();
+    }
 }
 
 module.exports = validarInput;
--- test/rutdom.spec.js
+++ test/rutdom.spec.js
@@ -13,9 +13,30 @@ global.navigator = {
 var $ = require('jquery');
 const RUTDom = require('../src/rutdom');
 
+console.log("Dir > " + __dirname);
+
 describe("Input valida rut", () => {
+    RUTDom("testito");
+
     it("Debe leer desde input", () => {
-        RUTDom("testito");
         chai.assert.exists($("#testito").change);
     })
-})
+
+    $("#testito").val("1234");
+
+    it("Debe poner un doge si falla", () => {
+        chai.assert.exists($("#errorDoge"));
+    })
+
+    $("#testito").val("12345");
+
+    it("Debe poner uno y solo un doge si falla", () => {
+        chai.assert.notTypeOf($("#errorDoge"), 'array');
+    })
+
+    $("#testito").val("16749892-2");
+
+    it("Debe no mostrar doge si rut es válido", () => {
+        chai.assert.notExists($("#errorDoge"));
+    })
+});
